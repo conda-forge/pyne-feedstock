cde16e60ea25748c448ff9356b106cdd1a999c06
Anthony Scopatz <scopatz@gmail.com>
2016-11-16 12:43:57 -0500

    excluding tests

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -6,4 +6,6 @@ export PATH="${PREFIX}/bin:${PATH}"
 # run tests
 test -f ${PREFIX}/lib/libpyne*
 test -f ${PREFIX}/include/pyne/pyne.h
-nosetests
+# exlcuding ENDF tests because CI no longer seems to be able to download the
+# files.
+nosetests -e="test_endf"
