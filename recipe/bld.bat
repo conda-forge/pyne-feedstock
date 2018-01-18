0bf56e4269dabaf2168cbe050c3118933af98d2b
xoviat <xoviat@users.noreply.github.com>
2018-01-18 16:58:45 -0600

    Windows first try

---
+++ recipe/bld.bat
@@ -0,0 +1,14 @@
+mkdir build
+cd build
+
+cmake -G "Ninja"              ^
+    -DCMAKE_BUILD_TYPE=Release              ^
+    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+    -DMOAB_INCLUDE_DIR="%LIBRARY_PREFIX%/include" ^
+    ..
+
+if errorlevel 1 exit 1
+
+cmake --build . --target install
+
+if errorlevel 1 exit 1
