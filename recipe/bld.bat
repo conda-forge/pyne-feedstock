adcde22ba7c3b9e9fd59c4b8955bf3c2eba981c7
xoviat <xoviat@users.noreply.github.com>
2018-01-18 17:00:06 -0600

    Fix build system

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,7 @@
 mkdir build
 cd build
 
-cmake -G "Ninja"              ^
+cmake -G "NMake Makefiles JOM"              ^
     -DCMAKE_BUILD_TYPE=Release              ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -DMOAB_INCLUDE_DIR="%LIBRARY_PREFIX%/include" ^
