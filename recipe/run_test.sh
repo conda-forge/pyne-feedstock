278f3748980f40440aa6e7c8a022f59de3d5fcc1
Paul P.H. Wilson <paul.wilson@wisc.edu>
2020-08-10 13:56:18 -0500

    add back the alara test since it works on linux

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -5,7 +5,7 @@ export PATH="${PREFIX}/bin:${PATH}"
 
 # It takes too long to download data files for CI
 rm -f test_endf.py xs/test_data_source.py test_fortran.py test_data_nofile.py \
-      test_materials_library.py test_alara.py
+      test_materials_library.py
 
 # run tests
 test -f ${PREFIX}/lib/libpyne*
