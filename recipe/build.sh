5e94ae46cf7826b8d6e831376d9b6e6021e00f25
Paul P.H. Wilson <paul.wilson@wisc.edu>
2020-08-10 11:11:38 -0500

    remove moab reference in build

--- recipe/build.sh
+++ recipe/build.sh
@@ -13,8 +13,6 @@ ${PYTHON} setup.py install \
   --build-type="Release" \
   --prefix="${PREFIX}" \
   --hdf5="${PREFIX}" \
-  --moab="${PREFIX}" \
-  -DMOAB_INCLUDE_DIR="${PREFIX}/include" \
   -DCMAKE_OSX_DEPLOYMENT_TARGET="${MACOSX_VERSION_MIN}" \
   ${skiprpath} \
   --clean \
